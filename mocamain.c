e993dbfb00ae99ad8abafd4ae1f5e6637cab033a
riccardofontanini <riccardo.font@hotmail.it>
2017-08-07 20:51:53 +0200

    Main classico... niente di speciale

--- mocamain.c
+++ mocamain.c
@@ -1,3 +1,13 @@
 /*
+    MoCA Montessoro Connector Application
+    Written by: Riccardo Fontanini
+    Start date: 7 August 2017
 
 */
+#include<stdlib.h>
+#include<stdio.h>
+
+void main(int argc, char[] argv )
+{
+
+}
